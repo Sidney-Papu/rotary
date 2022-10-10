a6eb5f2e54aa2e7f8e6a22e8d33672bda34770e2
sidney <sidney_papu@hotmail.com>
2022-10-10 10:02:32 -0300

    Coloquei sobre banco de cadeira de rodas

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="about">
-    <h1>página em construção</h1>
-    <h3>História do projeto</h3>
-    <h5>como começou</h5>
-    <h6>início</h6>
+  <div class="about"><h3>
+    Banco de Cadeira de Rodas
+  </h3>
+    
+    <h5>O Banco de Cadeiras de Rodas é um programa onde o Rotary Club Taquaritinga proporciona para a sua comunidade o empréstimo de Cadeiras de Rodas, Cadeiras Higiênicas, Muletas e Andadores, que tem como finalidade garantir o direito de cidadania dos portadores de necessidades especiais.
+
+O publico alvo são aqueles que não possuem condições financeiras em adquirir o equipamento, portanto o Rotary Club Taquaritinga empresta o equipamento por tempo indeterminado, enquanto for necessário, que posteriormente é devolvida para ser reemprestada para outros usuários.
+
+Os empréstimos são monitorados e administrados pelo gestor do banco de cadeiras de rodas do ano rotário em exercício.
+
+O programa existe há mais de 30 anos, e em homenagem ao Imortal Companheiro Manoel Ramalho, sempre imbuido em "Dar de Si antes de Pensar em Si", escolhemos o seu nome para apadrinhar esse programa, ficando portanto "Banco de Cadeira de Rodas Manoel Ramalho – Rotary Club Taquaritinga".
+
+Possuímos hoje aproximadamente 500 aparelhos nesse Banco, e para que o trabalho continue se renovando e crescendo, o Clube promove anualmente eventos, entre eles as noites da Batata Recheada, o Jantar da Sorte e a Quermesse no Distrito de Guariroba, onde contamos com a ajuda de todos os Companheiros que, solidariamente, trabalham nesses eventos.
+
+O Rotary Club Taquaritinga agradece o apoio de toda comunidade que está abraçando esta causa participando dos eventos destinados à este fim.</h5>
   </div>
 </template>
   
