903e0d3d89ee6fcbe17fed24590dc9bafdad0239
sidney <sidney_papu@hotmail.com>
2022-10-21 15:29:20 -0300

    alterações em index.js, ContatoView

---
+++ src/views/ContatoView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h4>
+            Informações de contato
+        </h4>
+    </div>
+    <h5>
+        Rua Francisco Mesquita, 640, Taquaritinga, SP, Brazil, 15900-000 
+        (16) 3252-6583 
+    </h5>
+</template>
+<script>
+export default {
+  name: 'ContatoView',
+  components: {
+  
+  }
+}
+</script>
+<style>
+
+</style>
--- src/views/contato.vue
+++
@@ -1,5 +0,0 @@
-<template>
-   <div>
-        <h4>
-            Informações de contato
-        </h4>  
